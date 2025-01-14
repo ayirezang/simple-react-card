e66a8c6b3b3431e13f26f675c6038adc24bfd474
ayirezang <patienceayirezang33@gmail.com>
2025-01-14 20:41:24 +0000

    simple-react-card

---
+++ src/cardComponent/src/App.jsx
@@ -0,0 +1,13 @@
+import { Card } from "@material-tailwind/react";
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <Card />
+      <h2>card title</h2>
+    </div>
+  );
+};
+
+export default App;
